3d5afbd971ed7e2095a150401b030279fc7d37ae
AndreaRantin2301 <andrea.rantin.01.23@gmail.com>
2025-07-01 14:47:56 +0200

    Changed borard to STM32F407VE

---
+++ variants/STM32F4xx/F407V(E-G)T_F417V(E-G)T/variant_generic.cpp
@@ -0,0 +1,112 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2020, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#if defined(ARDUINO_GENERIC_F407VETX) || defined(ARDUINO_GENERIC_F407VGTX) ||\
+    defined(ARDUINO_GENERIC_F417VETX) || defined(ARDUINO_GENERIC_F417VGTX)
+#include "pins_arduino.h"
+
+// Digital PinName array
+const PinName digitalPin[] = {
+  PB_11,
+  PB_10,
+  PB_8,
+  PD_12,
+  PD_13,
+  PD_14,
+  PD_15,
+  PC_6,
+  PD_11,
+  PC_7,
+  PC_8,
+  PB_15,
+  PB_14,
+  PB_13,
+  PC_9,
+  PA_8, //D15
+  PC_0,
+  PC_1,
+  PC_2,
+  PC_3,
+  PA_2,
+  PC_4, //A5
+  PB_2,
+  PB_1,
+  PB_0,
+  PA_7,
+  PA_6,
+  PA_5,
+  PA_4,
+  PA_3,
+  PE_14,
+  PE_13,
+  PE_11,
+  PE_9,
+  PE_15,
+  PE_12,
+  PE_8,
+  PE_7,
+  PE_2,
+  PE_3,
+  PE_4,
+  PE_5,
+  PE_6,
+  PC_13,
+  PC_14,
+  PC_15,
+  PH_0,
+  PH_1,
+  PA_0,
+  PA_1,
+  PC_5,
+  PB_12,
+  PD_8,
+  PD_9,
+  PD_10,
+  PA_9,
+  PA_10,
+  PA_11,
+  PA_12,
+  PA_13,
+  PA_14,
+  PA_15,
+  PC_10,
+  PC_11,
+  PC_12,
+  PD_0,
+  PD_1,
+  PD_2,
+  PD_3,
+  PD_4,
+  PD_5,
+  PD_6,
+  PD_7,
+  PB_3,
+  PB_4,
+  PB_5,
+  PB_6,
+  PB_7,
+  PB_9,
+  PE_0,
+  PE_1,
+};
+
+// Analog (Ax) pin number array
+const uint32_t analogInputPin[] = {
+  16,
+  17,
+  18,
+  19, 
+  20,
+  21,
+};
+
+#endif /* ARDUINO_GENERIC_* */
---
+++ variants/STM32F4xx/F407V(E-G)T_F417V(E-G)T/variant_generic.h
@@ -0,0 +1,253 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2020, STMicroelectronics
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ *******************************************************************************
+ */
+#pragma once
+
+/*----------------------------------------------------------------------------
+ *        STM32 pins number
+ *----------------------------------------------------------------------------*/
+#define PB11   0
+#define PB10   1
+#define PB8    2
+#define PD12   3
+#define PD13   4
+#define PD14   5
+#define PD15   6
+#define PC6    7
+#define PD11   8
+#define PC7    9
+#define PC8    10
+#define PB15   11
+#define PB14   12
+#define PB13   13
+#define PC9    14
+#define PA8    15  // D15
+#define PC0    16
+#define PC1    17
+#define PC2    18
+#define PC3    19
+#define PA2    20
+#define PC4    21  // A5
+#define PB2    22
+#define PB1    23
+#define PB0    24
+#define PA7    25
+#define PA6    26
+#define PA5    27
+#define PA4    28
+#define PA3    29
+#define PE14   30
+#define PE13   31
+#define PE11   32
+#define PE9    33
+#define PE15   34
+#define PE12   35
+#define PE8    36
+#define PE7    37
+#define PE2    38
+#define PE3    39
+#define PE4    40
+#define PE5    41
+#define PE6    42
+#define PC13   43
+#define PC14   44
+#define PC15   45
+#define PH0    46
+#define PH1    47
+#define PA0    48
+#define PA1    49
+#define PC5    50
+#define PB12   51
+#define PD8    52
+#define PD9    53
+#define PD10   54
+#define PA9    55
+#define PA10   56
+#define PA11   57
+#define PA12   58
+#define PA13   59
+#define PA14   60
+#define PA15   61
+#define PC10   62
+#define PC11   63
+#define PC12   64
+#define PD0    65
+#define PD1    66
+#define PD2    67
+#define PD3    68
+#define PD4    69
+#define PD5    70
+#define PD6    71
+#define PD7    72
+#define PB3    73
+#define PB4    74
+#define PB5    75
+#define PB6    76
+#define PB7    77
+#define PB9    78
+#define PE0    79
+#define PE1    80
+
+
+
+
+// Alternate pins number
+#define PA0_ALT1                (PA0  | ALT1)
+#define PA0_ALT2                (PA0  | ALT2)
+#define PA1_ALT1                (PA1  | ALT1)
+#define PA1_ALT2                (PA1  | ALT2)
+#define PA2_ALT1                (PA2  | ALT1)
+#define PA2_ALT2                (PA2  | ALT2)
+#define PA3_ALT1                (PA3  | ALT1)
+#define PA3_ALT2                (PA3  | ALT2)
+#define PA4_ALT1                (PA4  | ALT1)
+#define PA5_ALT1                (PA5  | ALT1)
+#define PA6_ALT1                (PA6  | ALT1)
+#define PA7_ALT1                (PA7  | ALT1)
+#define PA7_ALT2                (PA7  | ALT2)
+#define PA7_ALT3                (PA7  | ALT3)
+#define PA15_ALT1               (PA15 | ALT1)
+#define PB0_ALT1                (PB0  | ALT1)
+#define PB0_ALT2                (PB0  | ALT2)
+#define PB1_ALT1                (PB1  | ALT1)
+#define PB1_ALT2                (PB1  | ALT2)
+#define PB3_ALT1                (PB3  | ALT1)
+#define PB4_ALT1                (PB4  | ALT1)
+#define PB5_ALT1                (PB5  | ALT1)
+#define PB8_ALT1                (PB8  | ALT1)
+#define PB9_ALT1                (PB9  | ALT1)
+#define PB14_ALT1               (PB14 | ALT1)
+#define PB14_ALT2               (PB14 | ALT2)
+#define PB15_ALT1               (PB15 | ALT1)
+#define PB15_ALT2               (PB15 | ALT2)
+#define PC0_ALT1                (PC0  | ALT1)
+#define PC0_ALT2                (PC0  | ALT2)
+#define PC1_ALT1                (PC1  | ALT1)
+#define PC1_ALT2                (PC1  | ALT2)
+#define PC2_ALT1                (PC2  | ALT1)
+#define PC2_ALT2                (PC2  | ALT2)
+#define PC3_ALT1                (PC3  | ALT1)
+#define PC3_ALT2                (PC3  | ALT2)
+#define PC4_ALT1                (PC4  | ALT1)
+#define PC5_ALT1                (PC5  | ALT1)
+#define PC6_ALT1                (PC6  | ALT1)
+#define PC7_ALT1                (PC7  | ALT1)
+#define PC8_ALT1                (PC8  | ALT1)
+#define PC9_ALT1                (PC9  | ALT1)
+#define PC10_ALT1               (PC10 | ALT1)
+#define PC11_ALT1               (PC11 | ALT1)
+
+#define NUM_DIGITAL_PINS        82
+#define NUM_ANALOG_INPUTS       16
+
+// On-board LED pin number
+#ifndef LED_BUILTIN
+  #define LED_BUILTIN           PNUM_NOT_DEFINED
+#endif
+
+// On-board user button
+#ifndef USER_BTN
+  #define USER_BTN              PNUM_NOT_DEFINED
+#endif
+
+// SPI definitions
+#ifndef PIN_SPI_SS
+  #define PIN_SPI_SS            PA4
+#endif
+#ifndef PIN_SPI_SS1
+  #define PIN_SPI_SS1           PA15
+#endif
+#ifndef PIN_SPI_SS2
+  #define PIN_SPI_SS2           PNUM_NOT_DEFINED
+#endif
+#ifndef PIN_SPI_SS3
+  #define PIN_SPI_SS3           PNUM_NOT_DEFINED
+#endif
+#ifndef PIN_SPI_MOSI
+  #define PIN_SPI_MOSI          PA7
+#endif
+#ifndef PIN_SPI_MISO
+  #define PIN_SPI_MISO          PA6
+#endif
+#ifndef PIN_SPI_SCK
+  #define PIN_SPI_SCK           PA5
+#endif
+
+// I2C definitions
+#ifndef PIN_WIRE_SDA
+  #define PIN_WIRE_SDA          PB7
+#endif
+#ifndef PIN_WIRE_SCL
+  #define PIN_WIRE_SCL          PB6
+#endif
+
+// Timer Definitions
+// Use TIM6/TIM7 when possible as servo and tone don't need GPIO output pin
+#ifndef TIMER_TONE
+  #define TIMER_TONE            TIM6
+#endif
+#ifndef TIMER_SERVO
+  #define TIMER_SERVO           TIM7
+#endif
+
+// UART Definitions
+#ifndef SERIAL_UART_INSTANCE
+  #define SERIAL_UART_INSTANCE  4
+#endif
+
+// Default pin used for generic 'Serial' instance
+// Mandatory for Firmata
+#ifndef PIN_SERIAL_RX
+  #define PIN_SERIAL_RX         PA1
+#endif
+#ifndef PIN_SERIAL_TX
+  #define PIN_SERIAL_TX         PA0
+#endif
+
+// Extra HAL modules
+#if !defined(HAL_DAC_MODULE_DISABLED)
+  #define HAL_DAC_MODULE_ENABLED
+#endif
+#if !defined(HAL_ETH_MODULE_DISABLED)
+  #define HAL_ETH_MODULE_ENABLED
+#endif
+#if !defined(HAL_SD_MODULE_DISABLED)
+  #define HAL_SD_MODULE_ENABLED
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+  // These serial port names are intended to allow libraries and architecture-neutral
+  // sketches to automatically default to the correct port name for a particular type
+  // of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+  // the first hardware serial port whose RX/TX pins are not dedicated to another use.
+  //
+  // SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+  //
+  // SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+  //
+  // SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+  //
+  // SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+  //
+  // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+  //                            pins are NOT connected to anything by default.
+  #ifndef SERIAL_PORT_MONITOR
+    #define SERIAL_PORT_MONITOR   Serial
+  #endif
+  #ifndef SERIAL_PORT_HARDWARE
+    #define SERIAL_PORT_HARDWARE  Serial
+  #endif
+#endif
